9479c197445fccb3fcc860e36e4f08ab12d514d6
Denver <70090559+Denver44@users.noreply.github.com>
2021-12-24 23:11:47 +0530

    Serving Static Files Done

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import morgan from 'morgan';
+import path from "path";
 import { tourRouter, userRouter } from './routers/route.js'
 const app = express();
 
+const __dirname = path.resolve(path.dirname(''));
+
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
     req.requestedTime = new Date().toISOString();
     next();
@@ -16,7 +21,4 @@ app.use('/api/v1/users', userRouter);
 export default app;
 
 
-// Here for handling the route handlers we created a folder controllers as they called as controller only so we put all the handle router in that folder
-// We create a router folder and all the router is put there.
-// Here Server.js is our main file which will be run and start the server.
-// app.js is only for getting all the file and folder and then import to server.js and server.js will run and our sever will start.
+// To serve a static files we have to use express.static() middle ware where we have to pass the path of the folder which we want to server static after than we can serve the files of that folder
