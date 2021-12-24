2ccb62e4693e456bf9af7d77b0d6d9d37a037ec7
Denver <70090559+Denver44@users.noreply.github.com>
2021-12-24 12:47:45 +0530

    Handling POST Requests Done

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ import path from 'path'
 const app = express()
 const PORT = 5000;
 
+app.use(express.json()) // This is actually the middleware which actually help us to get the data from req now the data from req will be added to the body of req and then we can get the data in req object.
+
+
 const __dirname = path.resolve(path.dirname(''));
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+let tours = [];
+tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 
 app.get('/api/v1/tours', (req, res) => {
@@ -18,15 +22,24 @@ app.get('/api/v1/tours', (req, res) => {
     })
 })
 
+app.post('/api/v1/tours', (req, res) => {
+    const { body } = req
+    const id = tours[tours.length - 1].id + 1;
+    const tour = { ...body, id }
+    tours = [...tours, tour];
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: "created",
+            data: {
+                tour
+            }
+        })
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`server is started http://localhost:${PORT}`);
 })
 
-// Here we have creat a version for api like V1 so that in future if we create a new version so it will not do any braking changes to our client. we can still use the old version.
 
-// The function where we get res and req object are called request Handler.
-
-// Always use dirname and for that we have to import path module to resolve the dirname error and get the current dir path.
-
-// They way send the response to client is mostly used in industry so use this way only to send data to client side.
+// We have to call always async functions when we are saving data under a request handler.
