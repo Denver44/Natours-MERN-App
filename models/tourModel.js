d1f8fc1ea26078f039990691197733c491d5e31a
Denver <70090559+Denver44@users.noreply.github.com>
2021-12-31 15:34:04 +0530

    modelling the tours done

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,15 +5,53 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A Tour Must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    trim: true,
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A Tour Must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true, // THis will remove the whitespace from the end and start of the text
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
