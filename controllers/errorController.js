2bddfdaea39501072e888b1e7e605c613351508e
Denver <70090559+Denver44@users.noreply.github.com>
2022-01-04 13:05:07 +0530

    Handling Duplictae Databse fields error done

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -15,9 +15,8 @@ const sendErrorProd = (res, err) => {
       message: err.message || '',
     });
   } else {
-    console.log('ERROR 💥', err); // 1. Log Error
-    // 2. Send generic message
-    res.status(500).json({
+    /* 1. Log Error */ console.log('ERROR 💥', err);
+    /* 2. Send generic message */ res.status(500).json({
       status: 'error',
       message: 'Something went very wrong!',
     });
@@ -26,7 +25,12 @@ const sendErrorProd = (res, err) => {
 
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path} : ${err.value}.`;
-  return new AppError(message, 400); // 400 is for Bad Request
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const message = `Duplicate field value : /'${err.keyValue.name}'/. Please use another value`;
+  return new AppError(message, 400);
 };
 
 // eslint-disable-next-line no-unused-vars
@@ -36,6 +40,7 @@ export default (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
     if (err.name === 'CastError') error = handleCastErrorDB(error);
+    else if (err.code === 11000) error = handleDuplicateFieldsDB(error);
     sendErrorProd(res, error);
   }
 };
