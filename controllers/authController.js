250f042803fff3a51b87261166ad8810447aa7ca
Denver <70090559+Denver44@users.noreply.github.com>
2022-01-07 22:26:22 +0530

    JWT signing up user done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,23 @@
 /* eslint-disable no-unused-vars */
+import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
 import catchAsync from '../utils/catchAsync.js';
 
 const signUp = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    photo: req.body.photo,
+  });
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
