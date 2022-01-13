ad55d31612f66d93a44568b0aca7ab7ac25a6f25
Denver <70090559+Denver44@users.noreply.github.com>
2022-01-13 17:33:16 +0530

    rate limiter package installed and configured

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import express from 'express';
 import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
 import { tourRouter, userRouter } from './routers/route.js';
 import AppError from './utils/AppError.js';
 import GlobalErrorHandling from './controllers/errorController.js';
@@ -8,9 +9,20 @@ import GlobalErrorHandling from './controllers/errorController.js';
 const app = express();
 const __dirname = path.resolve(path.dirname(''));
 
+// 1. GLOBAL  MIDDLEWARE
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Adapt this upon your website requirement
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000, // 100 Request is allowed for One IP in one Hour
+  message: 'Too many request from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter); // This limiter will only affect the route with /api
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
