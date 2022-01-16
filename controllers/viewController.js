441819241cc4faab479499b02c9b97c944a5e88e
Denver <70090559+Denver44@users.noreply.github.com>
2022-01-16 16:16:12 +0530

    Tour page part 1 done

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import Tour from '../models/tourModel.js';
 import catchAsync from '../utils/catchAsync.js';
+import AppError from '../utils/AppError.js';
 
 const getOverview = catchAsync(async (req, res, next) => {
   // 1) Get Tour data from collection
@@ -14,10 +15,21 @@ const getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-const getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The forest Hiker Tour',
+const getTour = catchAsync(async (req, res, next) => {
+  // 1) Get Tour data from collection
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'reviews rating user',
   });
-};
+
+  // 2) Build template
+  // 3) Render that template using tour data from 1)
+  if (!tour) return new AppError('No such tour is available', 404);
+
+  return res.status(200).render('tour', {
+    title: 'The forest Hiker Tour',
+    tour,
+  });
+});
 
 export { getTour, getOverview };
--- routers/viewRoutes.js
+++ routers/viewRoutes.js
@@ -4,6 +4,8 @@ import { getTour, getOverview } from '../controllers/viewController.js';
 const router = express.Router();
 
 router.get('/', getOverview);
-router.get('/tour', getTour);
+
+// Adding slug like is using (:) is called URL Parameters
+router.get('/tour/:slug', getTour);
 
 export default router;
