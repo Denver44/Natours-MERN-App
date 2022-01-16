ea02351b3cb8369c17769e34c0ddba1fd636489d
Denver <70090559+Denver44@users.noreply.github.com>
2022-01-16 10:18:27 +0530

    Serving pug template done

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ import GlobalErrorHandling from './controllers/errorController.js';
 const app = express();
 const __dirname = path.resolve(path.dirname(''));
 
+//  Serving Static files
+app.use(express.static(path.join(__dirname, 'public'))); // Now we don't need to put the slashes and to view the pages in public folder : http://localhost:PORT/fileName.ext => http://localhost:PORT/index.html
+// app.use(express.static(`${__dirname}/public`));
+
+// Setting the Template Engine
+app.set('view engine', 'pug'); // Here we have set the template engine is pug so we don't need to put the extension when we render the templates, that's the benefit of setting template.
+app.set('views', path.join(__dirname, 'views')); // We have set the path of views folder.
+
 // 1. GLOBAL  MIDDLEWARE
 
 // Set Security http Headers
@@ -33,7 +41,8 @@ const limiter = rateLimit({
   message: 'Too many request from this IP, please try again in an hour!',
 });
 
-app.use('/api', limiter); // This limiter will only affect the route with /api
+// This limiter will only affect the route with /api and we can set different limiter for different route
+app.use('/api', limiter);
 
 // Body parser reading data from body into req.body, Here we have limited the body data to 10KB
 app.use(express.json({ limit: '10kb' }));
@@ -44,8 +53,7 @@ app.use(mongoSanitize());
 // Data Sanitization using XSS Clean
 app.use(xss());
 
-// Prevent Parameter Pollution, It will clear up the polluted query string
-// using whitelist we can allow the query which we want.
+// Prevent Parameter Pollution, It will clear up the polluted query string & using whitelist we can allow the query which we want.
 app.use(
   hpp({
     whitelist: [
@@ -59,13 +67,17 @@ app.use(
   })
 );
 
-//  Serving Static files
-app.use(express.static(`${__dirname}/public`));
-
 // Test Middleware
 app.use((req, res, next) => {
   req.requestedTime = new Date().toISOString();
   next();
+});
+
+// Rendering template file
+
+// http://localhost:5000/
+app.get('/', (req, res) => {
+  res.status(200).render('base');
 });
 
 app.use('/api/v1/tours', tourRouter);
@@ -80,3 +92,6 @@ app.all('*', (req, res, next) => {
 app.use(GlobalErrorHandling);
 
 export default app;
+
+// For serving static files in public folder
+// http://localhost:5000/ + fileName
