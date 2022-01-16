0df469e535a5053caee57f7212701a90180f7b6a
Denver <70090559+Denver44@users.noreply.github.com>
2022-01-16 13:11:19 +0530

    Tour template ready

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,11 +1,22 @@
+/* eslint-disable no-unused-vars */
+import Tour from '../models/tourModel.js';
+import catchAsync from '../utils/catchAsync.js';
+
+const getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get Tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build template
+  // 3) Render that template using tour data from 1)
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours,
+  });
+});
+
 const getTour = (req, res) => {
   res.status(200).render('tour', {
     title: 'The forest Hiker Tour',
-  });
-};
-const getOverview = (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours',
   });
 };
 
