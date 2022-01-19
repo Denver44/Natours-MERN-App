2ce78aa5aea53acb27c6195f3c5d5b0da63e06e8
Denver <70090559+Denver44@users.noreply.github.com>
2022-01-19 08:29:51 +0530

    Processing multiple images done

--- middleware/tourMiddleware.js
+++ middleware/tourMiddleware.js
@@ -1,5 +1,7 @@
 import multer from 'multer';
+import sharp from 'sharp';
 import AppError from '../utils/AppError.js';
+import catchAsync from '../utils/catchAsync.js';
 
 const aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -14,10 +16,38 @@ const multerStorage = multer.memoryStorage();
 // IMAGE PROCESSING
 
 // Here this time we will get re.files the plural from.
-const resizeTourImages = (req, res, next) => {
-  console.log('resizeTourImages', req.files);
+const resizeTourImages = catchAsync(async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) return next();
+
+  // 1. Process Image Cover
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333) // Width and Height set
+    .toFormat('jpeg') // Format change to jpeg
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+
+  // 2. Images
+
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, index) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${index + 1}.jpeg`;
+      await sharp(file.buffer)
+        .resize(2000, 1333) // Width and Height set
+        .toFormat('jpeg') // Format change to jpeg
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${filename}`);
+      req.body.images.push(filename);
+    })
+  );
+  // We sue map to save all the promise of the 3 images and then using promise.all we solve the promises and then we move to next middleware.
+  // If we don't have use await before promise.all we directly gone to next middleware.
+
   return next();
-};
+});
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
--- middleware/userMiddleware.js
+++ middleware/userMiddleware.js
@@ -1,6 +1,7 @@
 import multer from 'multer';
 import sharp from 'sharp';
 import AppError from '../utils/AppError.js';
+import catchAsync from '../utils/catchAsync.js';
 
 const setUserId = (req, res, next) => {
   req.params.id = req.user.id;
@@ -13,19 +14,19 @@ const multerStorage = multer.memoryStorage();
 // IMAGE PROCESSING
 
 // This is express middleware
-const resizeUserPhoto = (req, res, next) => {
+const resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next(); // if no file then go to next middleware
 
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`; // as we want filename because using this key we actually save the file in db check in updateMe Controller.
 
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
     .resize(500, 500) // Width and Height set
     .toFormat('jpeg') // Format change to jpeg
     .jpeg({ quality: 90 })
     .toFile(`public/img/users/${req.file.filename}`);
 
   return next();
-};
+});
 
 // Here we configure a filter which will check that data or the file is a image type or not.
 const multerFilter = (req, file, cb) => {
