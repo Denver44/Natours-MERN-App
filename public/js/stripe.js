900caef5a06caaf148cc146690a7c9829c8dd581
Denver <70090559+Denver44@users.noreply.github.com>
2022-01-20 10:14:54 +0530

    Small bug fix for payment for client side

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import axios from 'axios';
 import { showAlert } from './alerts';
-const stripe = Stripe('pk_test_BUkd0ZXAj6m0q0jMyRgBxNns00PPtgvjjr');
+const stripe = Stripe('pk_test_51KJaKjSEw2T9HrV8vPjYM8J7eyvLB40hBKz1ZhBMd0Dg4shc3uAckMSsKt8zAzcQEZSyyN1H5YPFCDk3GifoW23N00q4tF1mzR');
 
 export const bookTour = async tourId => {
   try {
